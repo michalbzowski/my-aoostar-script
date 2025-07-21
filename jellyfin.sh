dcbdfcd3d8681ee385d8867080bebf81584e165e
Michał Bzowski <michal.bzowski@gmail.com>
2025-07-21 19:00:47 +0200

    Update jellyfin.sh

--- jellyfin.sh
+++ jellyfin.sh
@@ -1,1 +1,5 @@
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+
 dnf install jellyfin
