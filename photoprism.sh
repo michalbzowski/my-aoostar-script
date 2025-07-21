4a3e1fdc7b18b7c11bfd3c283132db99b7fe1285
Michał Bzowski <michal.bzowski@gmail.com>
2025-07-21 15:34:22 +0200

    Update photoprism.sh

--- photoprism.sh
+++ photoprism.sh
@@ -1,1 +1,23 @@
+sudo partprobe /dev/sda
+sudo partx -a /dev/sda1
 sudo fdisk -l /dev/sda
+sudo mkdir -p /mnt/sda
+
+sudo blkid /dev/sda1
+sudo vi /etc/fstab
+#Dodaj do pliku wpis - uuid z blkid
+UUID=ab983751-a221-4d08-ae36-0f7b78ca5b5f /mnt/sda ext4 defaults 0 2
+systemctl daemon-reload
+
+
+
+
+sudo mount /dev/sda1 /mnt/sda
+sudo blkid /dev/sda1
+
+Następnie dodaj linię w /etc/fstab:
+
+text
+UUID=twoj-uuid /mnt/sda_data ext4 defaults 0 2
+
+
