53818a18d8226fd9895996c440f38c0a07be8b16
Michał Bzowski <michal.bzowski@gmail.com>
2025-07-21 15:46:43 +0200

    Update photoprism.sh

--- photoprism.sh
+++ photoprism.sh
@@ -6,18 +6,18 @@ sudo mkdir -p /mnt/sda
 sudo blkid /dev/sda1
 sudo vi /etc/fstab
 #Dodaj do pliku wpis - uuid z blkid
-UUID=ab983751-a221-4d08-ae36-0f7b78ca5b5f /mnt/sda ext4 defaults 0 2
+UUID=e6e75fe2-916c-4441-97fc-602e909b6bce /mnt/sda1 ext4 defaults 0 2
 systemctl daemon-reload
-
-
 
 
 sudo mount /dev/sda1 /mnt/sda
 sudo blkid /dev/sda1
 
-Następnie dodaj linię w /etc/fstab:
+Następnie dodaj linię w /etc/fstab: 
 
 text
 UUID=twoj-uuid /mnt/sda_data ext4 defaults 0 2
+Ale utworzenie partycji z web gui utworzyło wpis samo
 
+Podmieniam teraz konfigurację w docker compose, żeby voulmeny wskazywały na /mnt/sda1
 
