637d5de478d807de1fadb3cd1ac8fbcdd21b63a0
Michał Bzowski <michal.bzowski@gmail.com>
2025-07-22 02:39:02 +0200

    Update jellyfin.sh

--- jellyfin.sh
+++ jellyfin.sh
@@ -3,3 +3,7 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
 dnf install jellyfin
+
+sudo firewall-cmd --zone=FedoraServer --add-port=8096/tcp --permanent
+sudo firewall-cmd --add-service=jellyfin --permanent
+sudo firewall-cmd --reload
